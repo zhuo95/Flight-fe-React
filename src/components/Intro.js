423122c72a80baf1fe39a9e29b7a7a539c99a213
zhuo95 <34971718+zhuo95@users.noreply.github.com>
2018-06-19 18:08:06 -0400

    intro to web

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -43,16 +43,33 @@ class Intro extends React.Component {
                            <p>blablabla</p>
                            <p><a className="btn btn-primary btn-lg" href="/login">Get Started</a></p>
                        </div>
-                   </div>
+                       <div className="col-xs-12 col-md-5">
+                           <h3 className="entry-title">使用说明</h3>
+                           <div className="line"></div>
+                           <span className="post"> <i className="fas fa-calendar-alt"></i> 2018.06.15 by zz</span>
+                           <p className="para">新生申请接机: 请点击注册，填写个人信息(家乡，毕业院校等)，登陆后填写航班信息等，申请完成后，会进行匹配。接机人会根据接机时间等选择需要接机的新同学
+                           ,匹配完成后会将微信号邮箱等联系方式发送给新同学。</p>
+                           <p className="para">接机人申请: 注册时选择提供帮助，只支持edu邮箱注册，注册完成后登录即可浏览所有的接机请求，和新生个人信息,可选择提供帮助，点击提供帮助后会将您的联系方式发送给新生。</p>
+                       </div>
 
-                   <div className="container">
-                       <div className="entry">
+                       <div className="col-xs-0 col-xs-2"></div>
+
+                       <div className="col-xs-12 col-md-5">
                            <h3 className="entry-title">Intro to this web</h3>
                            <div className="line"></div>
                            <span className="post"> <i className="fas fa-calendar-alt"></i> 2018.06.15 by zz</span>
-                           <p className="para">This is a website that helps you ...</p>
+                           <p className="para">For new student: sign up as new student, fill in personal information(hometown, school,etc.),
+                               login to fill in the form about your flight. After you submit the form, senior students will select the new students who need help.</p>
+                           <p className="para">For senior students: sign up as senior student, only edu emails are supported, after registration, you can login to
+                            view all pick-up requests, you can choose to provide help. After you click help, your contact info will be sent to
+                            the new student.</p>
                        </div>
+
                    </div>
+
+                   <p className="copyright">
+                       2018-06
+                   </p>
 
 
                </ReactCSSTransitionGroup>
